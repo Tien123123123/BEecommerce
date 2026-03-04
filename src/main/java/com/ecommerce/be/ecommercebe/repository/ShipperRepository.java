f1684270d9d3100d9b866035f74f57d58685441b
Trinh Gia Tien <giatien611@gmail.com>
2026-03-04 20:16:22 +0700

    fix Shipper Service and Repo

--- src/main/java/com/ecommerce/be/ecommercebe/repository/ShipperRepository.java
+++ src/main/java/com/ecommerce/be/ecommercebe/repository/ShipperRepository.java
@@ -2,6 +2,25 @@ package com.ecommerce.be.ecommercebe.repository;
 
 import com.ecommerce.be.ecommercebe.model.ShipperEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ShipperRepository extends JpaRepository<ShipperEntity, Long> {
+
+    @Query(nativeQuery = true,
+            value = "select * from shippers where id = :userId limit 1"
+    )
+    ShipperEntity getShipperIncludeDeleted(@Param("userId") Long userId);
+
+    @Query(nativeQuery = true,
+            value = "select soft_delete from shippers where id = :userId"
+    )
+    Boolean isSoftDeleted(@Param("userId") Long userId);
+
+    @Modifying
+    @Query(nativeQuery = true,
+            value = "update shippers set soft_delete = false where id = :userId"
+    )
+    void restoreById(@Param("userId") Long userId);
 }
--- src/main/java/com/ecommerce/be/ecommercebe/service/ShipperService.java
+++ src/main/java/com/ecommerce/be/ecommercebe/service/ShipperService.java
@@ -3,6 +3,7 @@ package com.ecommerce.be.ecommercebe.service;
 import com.ecommerce.be.ecommercebe.constant.user.UserStatus;
 import com.ecommerce.be.ecommercebe.dto.request.ShipperDTORequest;
 import com.ecommerce.be.ecommercebe.dto.response.ShipperResponse;
+import com.ecommerce.be.ecommercebe.dto.response.UserResponse;
 import com.ecommerce.be.ecommercebe.dto.response.mapper.ShipperMapper;
 import com.ecommerce.be.ecommercebe.model.ShipperEntity;
 import com.ecommerce.be.ecommercebe.model.UserEntity;
@@ -14,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +24,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ShipperService {
     private static final Logger logger = LoggerFactory.getLogger(ShipperService.class);
-    private final UserRepository userRepository;
 
     private final ShipperRepository shipperRepository;
 
@@ -52,4 +53,42 @@ public class ShipperService {
 
         return shipperMapper.toDTO(saved);
     }
+
+    public ShipperEntity getShipper(Long userId){
+        return shipperRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Cannot find shipper by id " + userId));
+    }
+    @Cacheable(value = "shippers", key = "#userId", unless = "#result == null")
+    public ShipperResponse getShipperDetail(Long userId){
+        return shipperMapper.toDTO(getShipper(userId));
+    }
+
+    @CacheEvict(value = "shippers", key = "#userId")
+    public void deleteShipper(Long userId){
+        UserEntity user = userService.getUser(userId);
+        ShipperEntity shipperEntity = getShipper(user.getId());
+
+        // Validate
+
+        //
+        shipperRepository.delete(shipperEntity);
+    }
+
+    @CacheEvict(value = "shippers", key = "#userId")
+    public ShipperResponse restoreShipper(Long userId){
+        UserEntity user = userService.getUser(userId);
+
+        // Validate
+        Boolean isDeleted = shipperRepository.isSoftDeleted(user.getId());
+        if(Boolean.TRUE.equals(isDeleted)){
+            shipperRepository.restoreById(userId);
+        }else{
+            throw new RuntimeException("Shipper " + user.getId() + " is not banned!");
+        }
+        //
+        ShipperEntity restored = shipperRepository.getShipperIncludeDeleted(userId);
+
+        return shipperMapper.toDTO(restored);
+    }
+
 }
